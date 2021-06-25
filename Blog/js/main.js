ea6527f1ce75d79ff1e1e7d2f751c82ab1a2cddd
Anna <november11131999@gmail.com>
2021-06-25 14:53:23 +0300

    menu burger

--- Blog/js/main.js
+++ Blog/js/main.js
@@ -35,6 +35,17 @@ testWebP(function (support) {
 var isMobile = { Android: function () { return navigator.userAgent.match(/Android/i); }, BlackBerry: function () { return navigator.userAgent.match(/BlackBerry/i); }, iOS: function () { return navigator.userAgent.match(/iPhone|iPad|iPod/i); }, Opera: function () { return navigator.userAgent.match(/Opera Mini/i); }, Windows: function () { return navigator.userAgent.match(/IEMobile/i); }, any: function () { return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows()); } };
 if (isMobile.any()) {
 	document.body.classList.add('_touch');
+
+	let menuArrows = document.querySelectorAll('.arrow-down');
+	if (menuArrows.length > 0) {
+		for (let index = 0; index < menuArrows.length; index++) {
+			const menuArrow = menuArrows[index];
+			menuArrow.addEventListener('click', function () {
+				menuArrow.parentElement.classList.toggle('_active-menu');
+			});
+		}
+	}
+
 } else {
 	document.body.classList.add('_pc');
 }
