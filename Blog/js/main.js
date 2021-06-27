dae727430bbf9c3f7f4942e71784e9d7b33e179d
Anna <november11131999@gmail.com>
2021-06-27 15:00:33 +0300

    service block

--- Blog/js/main.js
+++ Blog/js/main.js
@@ -93,6 +93,23 @@ $(document).ready(function () {
 		draggable: false
 	});
 
+	//For svg image
+	$('img.img-svg').each(function () {
+		var $img = $(this);
+		var imgClass = $img.attr('class');
+		var imgURL = $img.attr('src');
+		$.get(imgURL, function (data) {
+			var $svg = $(data).find('svg');
+			if (typeof imgClass !== 'undefined') {
+				$svg = $svg.attr('class', imgClass + ' replaced-svg');
+			}
+			$svg = $svg.removeAttr('xmlns:a');
+			if (!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+				$svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+			}
+			$img.replaceWith($svg);
+		}, 'xml');
+	});
 
 	//ZOOM
 	if ($('.gallery').length > 0) {
